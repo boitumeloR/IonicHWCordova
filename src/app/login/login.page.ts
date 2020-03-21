0aa7a877bfdac03a4d56feee494f950d5486fe6f
Boitumelo Rampete <u17120323@tuks.co.za>
2020-03-21 11:43:58 +0200

    Add Cordova

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService, Session } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Storage } from '@ionic/storage';
 
 export interface Login {
   Username: string;
@@ -23,19 +24,21 @@ export class LoginPage implements OnInit {
   };
   returnType: Observable<Session>;
   error: string;
-  constructor(private serv: AuthService, private router: Router) { }
+  constructor(private serv: AuthService, private router: Router, private store: Storage) { }
 
   ngOnInit() {
   }
 
   Login() {
-    this.router.navigateByUrl('tabs/tabs/tab1');
-    /*console.log(this.login);
+    console.log(this.login);
     this.returnType = this.serv.Login(this.login);
     this.returnType.subscribe(data => {
       if (data.Error === null) {
-        this.router.navigateByUrl('/tabs/tab1');
+        this.router.navigateByUrl('/tabs/tabs/tab1');
+        this.store.set('session', JSON.stringify(data));
+      } else {
+        this.error = data.Error;
       }
-    }); */
+    });
   }
 }
